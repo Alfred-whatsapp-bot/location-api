9c924998cbd3ab23608cf83a8030766219ef2b6e
Luis Felipe Amorim Sant'Ana <1luisfelipe2@gmail.com>
2022-09-30 16:20:14 -0400

    versao final

--- server.js
+++ server.js
@@ -7,25 +7,21 @@ const cors = require("cors"); //npm i --save cors
 const mysql = require("mysql"); //npm i --save mysql
 require("dotenv").config(); //npm i --save dotenv
 const config = {
-  user: process.env.DB_USER, // e.g. 'my-db-user'
-  password: process.env.DB_PASS, // e.g. 'my-db-password'
-  database: process.env.DB_NAME, // e.g. 'my-database'
-  socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
-  // Specify additional properties here.
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  socketPath: `/cloudsql/${process.env.INSTANCE_UNIX_SOCKET}`,
   waitForConnections: true,
   queueLimit: 0,
 };
 const connection = mysql.createConnection(config, { multipleStatements: true });
 app.use(cors());
 
-// API key
-const apiKey = process.env.APIKEY;
-
 // Get the user's location
 app.get("/location", (req, res) => {
   const { address } = req.query;
   fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.APIKEY}`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -54,6 +50,6 @@ app.get("/location", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log("Server started on port" + process.env.PORT)
-);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log("Server started on port" + PORT));
