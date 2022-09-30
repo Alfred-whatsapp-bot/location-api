232e7c281f51fae066ba30a54098f9b3a9c5afb5
Luis Felipe Amorim Sant'Ana <1luisfelipe2@gmail.com>
2022-09-30 14:47:04 -0400

    ajustes

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.get("/location", (req, res) => {
       }
     });
 });
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server started on port 3000")
+app.listen(process.env.PORT || 8080, () =>
+  console.log("Server started on port" + process.env.PORT)
 );
